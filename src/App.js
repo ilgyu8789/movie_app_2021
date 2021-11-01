637ebf69f88fc0ca29a1b5ccb9f32d194df18a4e
박일규 <t5nn123@naver.com>
2021-11-01 17:45:09 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import React from "react";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
   },
   {
+    id: 2,
     name: "samgyetang",
     image:
       "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
   },
 ];
 
-function App() {
+function Food({ name, picture }) {
   return (
     <div>
-      {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
+function App() {
+  return (
+    <div>
+      {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
+    </div>
+  );
+}
 export default App;
