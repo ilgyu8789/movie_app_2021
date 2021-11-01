2d73e94cba8dd307ab881fe09512fc6349ba5bbd
박일규 <t5nn123@naver.com>
2021-11-01 19:13:02 +0900

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
@@ -6,29 +7,42 @@ const foodILike = [
     name: "Kimchi",
     image:
       "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 5,
   },
   {
     id: 2,
     name: "samgyetang",
     image:
       "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 4.3,
   },
 ];
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   );
 }
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
