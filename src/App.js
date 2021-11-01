e1974ae23995261b9ff0c951dcc183de40d5d307
박일규 <t5nn123@naver.com>
2021-11-01 17:30:41 +0900

    Dynamic Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from "react";
 
-function Food({ favourite }) {
-  return <h2>I love {favourite}</h2>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+  },
+  {
+    name: "samgyetang",
+    image:
+      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!</h1>
-      <Food favourite="kimchi" />
-      <Food favourite="ramen" />
-      <Food favourite="samgiopsal" />
-      <Food favourite="chukumi" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
